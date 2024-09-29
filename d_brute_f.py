d6f1114ad443d956765b2b8167fd50abe1f17edf
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-09-30 07:00:05 +0900

    Update logic

--- d_brute_f.py
+++ d_brute_f.py
@@ -1,18 +1,18 @@
 from urllib.request import urlopen
-from urllib.request import HTTPError
+from urllib.request import HTTPError, URLError
 from time import sleep
 from urllib.request import Request
 import csv
 import ssl
 
-
+# 'wordlists/directory-list-custom.txt'
 # 'wordlists/directory-list-2.3-small.txt'
 with open('wordlists/directory-list-custom.txt', "r") as File:
     data = [line.strip() for line in File.readlines()]
 
 founded_urls = []
 for i in data:
-    url = "http://127.0.0.1/community_site/"
+    url = "http://127.0.0.1:3000/"
     url += i.strip()
     req = Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0')
@@ -23,31 +23,44 @@ for i in data:
 
         context = ssl._create_unverified_context()
         a = urlopen(req, context=context)
-
-        if a.getcode() in [301, 302]:
-            print(f"[+] Redirected: {url} -> {a.geturl()}")
-        elif a.getcode() == 403:
-            print(f"[+] Forbidden but Exists: {url}")
-        else:
-            print(f"[+] Found: {url}")
+    
+        print(f"[+] Found: {url}")
+        if url not in founded_urls:
             founded_urls.append(url)
+            
     except HTTPError as e:
+        if e.code in [301, 302]:
+            print(f"[+] Redirected: {url} -> {e.headers['Location']}")
+            founded_urls.append(url)
+        elif e.code == 403:
+            print(f"[+] Forbidden but Exists: {url}")
+            founded_urls.append(url)
         if e.code == 429:  # Too Many Requests
             print(f"[-] Rate limit hit. Slowing down...")
             sleep(5)
         # print(f"[-] Error {e.code}: {e.reason}")
         continue
 
+    except URLError as e:
+        print(f"[-] Failed to reach server: {url}")
+        print(f"Reason: {e.reason}")
+        continue
+
+print("")
 print("----------------------------------------------------------------------------------")
-print("[+] Found URLs")
+print("")
 
-for j in founded_urls:
-    print(j)
+if founded_urls:
+    print("[+] Found URLs")
+    for j in founded_urls:
+        print(j)
 
-with open('found_urls.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Found URLs"])
-    for url in founded_urls:
-        writer.writerow([url])
+    with open('csv_files/found_urls.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Found URLs"])
+        for url in founded_urls:
+            writer.writerow([url])
+else:
+    print('[*] Done! but nothing found.')
 
 File.close()
